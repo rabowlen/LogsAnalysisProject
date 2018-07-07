d1a2e08b6fe656dcc1e89f4ff8c0ce3d4ae847f6
Ryan Bowlen <rabowlen@gmail.com>
2018-07-07 13:31:33 -0500

    Update newsdb.py
    Removed whitespace and rewrote a SQL query.

--- newsdb.py
+++ newsdb.py
@@ -5,11 +5,12 @@ import psycopg2
 # 3 most popular articles
 query_1_desc = ("Three most popular articles:")
 query_1 = (
-    "select articles.title, count(*) as views "
-    "from articles inner join log on log.path "
-    "like concat('%', articles.slug, '%') "
-    "where log.status like '%200%' group by "
-    "articles.title, log.path order by views desc limit 3")
+    "select title, count(*) as views "
+       "from log join articles "
+       "on log.path = concat('/article/', articles.slug) "
+       "group by title "
+       "order by views desc "
+       "limit 3;")
 
 # Most popular authors
 query_2_desc = ("Most popular authors:")
@@ -18,7 +19,7 @@ query_2 = (
     "join authors on articles.author = authors.id inner join log "
     "on log.path like concat('%', articles.slug, '%') where "
     "log.status like '%200%' group "
-    "by authors.name order by views desc")
+    "by authors.name order by views desc;")
 
 # Days with more than 1% errors
 query_3_desc = ("Days where more than one percent of requests led to errors.")
@@ -29,7 +30,7 @@ query_3 = (
     "perc from (select substring(cast(log.time as text), 0, 11) as day, "
     "count(*) as requests from log where status like '%404%' group by day)"
     "as log_percentage group by day order by perc desc) as final_query "
-    "where perc >= 1")
+    "where perc >= 1;")
 
 
 def connect(database_name="news"):
@@ -49,13 +50,13 @@ def get_query_results(query):
 
 
 def print_query_results(query_results):
-    print (query_results[1])
+    print(query_results[1])
     for index, results in enumerate(query_results[0]):
         print "\t", index+1, "-", results[0], "\t-", str(results[1]), "views"
 
 
 def print_error_results(query_results):
-    print (query_results[1])
+    print(query_results[1])
     for results in query_results[0]:
         print "\t", results[0], "-", str(results[1]) + "% errors"
 
